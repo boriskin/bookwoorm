88c439e8cd0c95d871be8e0452faa188e1058f81
Pavel Boriskin <paboriskin@gmail.com>
2020-02-23 13:58:19 +0300

    Add crud for Author and middleName nullable=true

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AuthorRepository")
@@ -24,12 +25,14 @@ class Author
     private $firstName;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
      */
     private $middleName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     *
      */
     private $lastName;
 
